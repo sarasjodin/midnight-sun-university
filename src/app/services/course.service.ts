9bd0cefa0971170076e5301eea7b17b19c8bac5c
La svedese <5554930+sarasjodin@users.noreply.github.com>
2026-08-25 15:19:54 +0200

    feat: load and display course data

--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -1,4 +1,15 @@
-import { Service } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
-@Service()
-export class CourseService {}
+import { Course } from '../models/course';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CourseService {
+  private readonly http = inject(HttpClient);
+
+  getCourses() {
+    return this.http.get<Course[]>('/data/miun_courses.json');
+  }
+}
